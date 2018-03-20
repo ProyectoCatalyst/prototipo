9468f19d4029a593879460a8c5608682ca5e1c4d
Monserrat Gonzalez <monse.gu21@gmail.com>
2018-03-19 20:22:28 -0600

    Correción de clase
    Correción de clase

--- public/clases.js
+++ public/clases.js
@@ -36,7 +36,7 @@ class EncargadoAduanas extends Usuario {
   }
 }
 
-class   extends Usuario {
+class EncargadoSucursales extends Usuario {
   constructor(pnombre, psegundoNombre, pprimerApellido, psegundoApellido, pcedula, pfecha, pgenero, pubicacion, pprovincia, pcanton, pdistrito, pdireccion, pcorreo, pcontrasenna, prol) {
     super(pnombre, psegundoNombre, pprimerApellido, psegundoApellido, pcedula, pfecha, pgenero, pubicacion, pprovincia, pcanton, pdistrito, pdireccion, pcorreo, pcontrasenna, prol)
   }
