9f4607564c279180941a390b0e9b136891e46b87
Rolo <rolanc@gmail.com>
2018-03-25 21:34:34 -0600

    Integracion
    Integracion al master

--- public/components/servicios/usuarios.service.js
+++ public/components/servicios/usuarios.service.js
@@ -14,7 +14,8 @@
       agregarUsuario: _agregarUsuario,
       obtenerlistadeusuarios: _obtenerlistadeusuarios,
       obtenerlistadeFiltrada: _obtenerListaFiltrada,
-      desactivarCuenta: _desactivarCuenta
+      actualizarUsuario: _actualizarUsuario,
+      obtenerListaPorEstados: _obtenerListaPorEstados
     };
     return publicAPI; 
 
@@ -51,21 +52,28 @@
 
           switch(obj.rol){
             case 2:
-              let tempEncargadoAduana = new EncargadoAduanas(obj.primerNombre, obj.segundoNombre, obj.primerApellido, obj.segundoApellido, obj.cedula, tempfecha, obj.genero, obj.ubicacion, obj.provincia, obj.canton, obj.distrito, obj.direccion, obj.correo, obj.contrasenna, obj.rol, obj.estadoDesactivado);
+              let tempEncargadoAduana = new EncargadoAduanas(obj.primerNombre, obj.segundoNombre, obj.primerApellido, obj.segundoApellido, obj.cedula, tempfecha, obj.genero, obj.ubicacion, obj.provincia, obj.canton, obj.distrito, obj.direccion, obj.correo, obj.contrasenna, obj.rol, obj.estado);
 
               listadeusuarios.push(tempEncargadoAduana);
             break;
 
             case 3:
-              let tempEncargadoSucursal = new EncargadoSucursales(obj.primerNombre, obj.segundoNombre, obj.primerApellido, obj.segundoApellido, obj.cedula, tempfecha, obj.genero, obj.ubicacion, obj.provincia, obj.canton, obj.distrito, obj.direccion, obj.correo, obj.contrasenna, obj.rol, obj.estadoDesactivado);
+              let tempEncargadoSucursal = new EncargadoSucursales(obj.primerNombre, obj.segundoNombre, obj.primerApellido, obj.segundoApellido, obj.cedula, tempfecha, obj.genero, obj.ubicacion, obj.provincia, obj.canton, obj.distrito, obj.direccion, obj.correo, obj.contrasenna, obj.rol, obj.estado);
 
               listadeusuarios.push(tempEncargadoSucursal);
             break;
 
             case 5:
-              let tempCliente = new Cliente(obj.primerNombre, obj.segundoNombre, obj.primerApellido, obj.segundoApellido, obj.cedula, tempfecha, obj.genero, obj.ubicacion, obj.provincia, obj.canton, obj.distrito, obj.direccion, obj.correo, obj.contrasenna, obj.rol, obj.estadoDesactivado, obj.telefono);
+              let tempCliente = new Cliente(obj.primerNombre, obj.segundoNombre, obj.primerApellido, obj.segundoApellido, obj.cedula, tempfecha, obj.genero, obj.ubicacion, obj.provincia, obj.canton, obj.distrito, obj.direccion, obj.correo, obj.contrasenna, obj.rol, obj.estado, obj.telefono);
 
               listadeusuarios.push(tempCliente);
+            break;
+
+            default:
+
+              let tempUsuario = new Usuario(obj.primerNombre, obj.segundoNombre, obj.primerApellido, obj.segundoApellido, obj.cedula, tempfecha, obj.genero, obj.ubicacion, obj.provincia, obj.canton, obj.distrito, obj.direccion, obj.correo, obj.contrasenna, obj.rol, obj.estado);
+
+              listadeusuarios.push(tempUsuario);
             break;
           }
         });
@@ -74,10 +82,19 @@
       return listadeusuarios;
     }
 
+    function _actualizarUsuario (pusuarioModificado){
+      let listadeusuarios = _obtenerlistadeusuarios();
 
-    function _obtenerListaTarjetas (){
+      for (let i = 0; i < listadeusuarios.length; i++){
+        if(listadeusuarios[i].getCorreo() == pusuarioModificado.getCorreo()){
+          listadeusuarios[i] = pusuarioModificado;
+        }
+      }
 
-    };
+      let modificacionExitosa = localStorageFactory.setItem(listaUsuarios, listadeusuarios);
+      
+      return modificacionExitosa;
+    }
 
     function _obtenerListaFiltrada(pnumrol){
       let listadeusuarios = _obtenerlistadeusuarios(),
@@ -92,18 +109,18 @@
       return listaFiltrada;
     }
 
-    function _desactivarCuenta(pcorreo) {
+    function _obtenerListaPorEstados(pestado) {
       let listadeusuarios = _obtenerlistadeusuarios(),
-          desactivar = false;
+          listaFiltrada = [];
 
       for(let i = 0; i < listadeusuarios.length; i++){
-        if(listadeusuarios[i].getCorreo() == pcorreo){
-          listadeusuarios[i].setEstado(true);
-          desactivar = true;
+        if(listadeusuarios[i].getEstado() == pestado){
+          listaFiltrada.push(listadeusuarios[i]);
         }
       }
-      return desactivar;
+      return listaFiltrada;
     }
+    
   };
 
 })();
