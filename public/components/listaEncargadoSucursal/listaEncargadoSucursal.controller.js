5cdfcdb9818054157c9bfd049fa05fc3311b4895
varo92 <35508250+varo92@users.noreply.github.com>
2018-03-26 16:58:09 -0600

    arreglando listar enc sucursal

--- public/components/listaEncargadoSucursal/listaEncargadoSucursal.controller.js
+++ public/components/listaEncargadoSucursal/listaEncargadoSucursal.controller.js
@@ -9,15 +9,26 @@
   function controladorListaEncargadoSucursal($state, $stateParams, servicioUsuarios) {
     let vm = this;
 
-    vm.listarEncargadoSucursal = servicioUsuarios.obtenerlistadeusuarios();
+    vm.listarEncargadoSucursal = servicioUsuarios.obtenerlistadeFiltrada(3);
+
+
+  }
+})();
+
 
      //   vm.listarEncargadoSucursal = () => {
     //    let encargadosSucursal = servicio.retornarEncargadosSucursal(datosCliente), // 
    
     //   return encSucursal // retorna todos los paquetes que se encontraron en estado prealerta
 
-    vm.obtenerlistadeusuarios = servicioUsuarios.obtenerlistadeusuarios();
 
-  }
-})();
-
+  //  function retonarListaEncargadosSucursal(){
+  //     let listaEncargadosSucursal = [];
+  //     for(let i=0;i<vm.listadeusuarios.length;i++){
+  //       let usuario = vm.listadeusuarios[i];
+  //       if(usuarioTemp.getRol() == 3){
+  //           listaEncargadosSucursal.push(usuarioTemp);
+  //       }
+  //     }
+  //     return listaEncargadosSucursal;
+  //  }
